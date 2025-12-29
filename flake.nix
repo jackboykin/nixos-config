6d2195898af105e07b207aa7c442463d5c83a4ab
Jack Boykin <jtboykin.jb@gmail.com>
2025-12-29 00:39:55 -0600

    Configure flakes and basic system setup

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "My NixOS Flake Configuration";
+
+  inputs = {
+    # Pin to the 25.11 release as per your stateVersion
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos-orion = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
