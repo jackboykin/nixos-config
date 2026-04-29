c9b24c5f910dd5f5f8ff73b9320cba4e188f2a7b
Jack Boykin <jtboykin.jb@gmail.com>
2026-04-29 17:35:40 -0500

    trim shell aliases, add nh boot shortcuts, inline shell.nix, bump inputs

--- users/jack/user.nix
+++ users/jack/user.nix
@@ -1,13 +1,24 @@
 {username, ...}: {
-  imports = [
-    ./programs/programs.nix
-    ./shell.nix
-  ];
+  imports = [./programs/programs.nix];
 
   home = {
     inherit username;
     homeDirectory = "/home/${username}";
     stateVersion = "26.05";
+    sessionPath = [
+      "$HOME/.cargo/bin"
+      "$HOME/.local/bin"
+    ];
+    shellAliases = {
+      q = "exit";
+      nr = "nh os switch";
+      nru = "nh os switch -u";
+      nb = "nh os boot";
+      nbu = "nh os boot -u";
+      l = "eza --icons -la --no-user --no-time --no-permissions --git --group-directories-first";
+      lr = "eza --icons -laR --git-ignore --git --no-user --no-time --no-permissions --group-directories-first";
+      t = "eza --icons --tree --git-ignore";
+    };
   };
 
   xdg.enable = true;
