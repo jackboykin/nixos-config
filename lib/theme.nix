605049cb9311060de943254ae8fa36bf5e517713
Jack Boykin <jtboykin.jb@gmail.com>
2026-06-25 05:55:00 -0500

    theme: rework palette

--- lib/theme.nix
+++ lib/theme.nix
@@ -45,33 +45,33 @@ with lib; let
     rgbToHex mix;
 
   colors = rec {
-    color0 = "#36302f";
-    color1 = "#ee0606";
-    color2 = "#35de4f";
-    color3 = "#ff9000";
-    color4 = "#286ef1";
-    color5 = "#e52195";
-    color6 = "#22e1ef";
-    color7 = "#dae4e4";
-    color8 = "#9a8b94";
-    color9 = "#ff5500";
-    color10 = "#7ed37d";
-    color11 = "#ffaf2f";
-    color12 = "#6357f9";
-    color13 = "#ec8cc5";
-    color14 = "#69e8c8";
-    color15 = "#e5e9ef";
+    color0 = "#262320";
+    color1 = "#e0707f";
+    color2 = "#a9b58a";
+    color3 = "#ffc799";
+    color4 = "#8aa0b8";
+    color5 = "#ff2d96";
+    color6 = "#93c2bd";
+    color7 = "#d7d2c8";
+    color8 = "#6f6a60";
+    color9 = "#ff8a99";
+    color10 = "#c2cda3";
+    color11 = "#ffd6b0";
+    color12 = "#a6bcd4";
+    color13 = "#ff5cae";
+    color14 = "#b0dad5";
+    color15 = "#ece8df";
 
-    foreground = "#dae4e4";
-    background = "#0b0a09";
+    foreground = "#d7d2c8";
+    background = "#0d0c0b";
 
     base00 = background;
-    base01 = "#1a1716";
+    base01 = "#1a1817";
     base02 = color0;
     base03 = color8;
-    base04 = "#7b6562";
+    base04 = "#8f897c";
     base05 = foreground;
-    base07 = "#fce4f0";
+    base07 = "#f4e4d2";
 
     black = color0;
     red = color1;
@@ -90,14 +90,14 @@ with lib; let
     brightCyan = color14;
     brightWhite = color15;
 
-    orange = color9;
-    maroon = color9;
+    orange = "#ffae6a";
+    maroon = orange;
     sky = blue;
     teal = cyan;
-    purple = brightBlue;
+    purple = magenta;
     base = base00;
     mantle = base01;
-    crust = "#050403";
+    crust = "#060605";
     surface0 = base01;
     surface1 = base02;
     surface2 = base03;
@@ -113,7 +113,7 @@ with lib; let
     info = blue;
     hint = cyan;
 
-    gitAdded = green;
+    gitAdded = magenta;
     gitModified = yellow;
     gitDeleted = red;
 
@@ -123,11 +123,11 @@ in {
   inherit colors mixColors hexToRgb;
 
   diff = {
-    hunkHeader = mixColors colors.base colors.purple 0.8;
+    hunkHeader = mixColors colors.base colors.cyan 0.8;
     minusEmph = mixColors colors.base colors.red 0.6;
     minus = mixColors colors.base colors.red 0.8;
-    plusEmph = mixColors colors.base colors.green 0.6;
-    plus = mixColors colors.base colors.green 0.8;
+    plusEmph = mixColors colors.base colors.magenta 0.6;
+    plus = mixColors colors.base colors.magenta 0.8;
     maroon = mixColors colors.base colors.maroon 0.6;
     blue = mixColors colors.base colors.blue 0.6;
     cyan = mixColors colors.base colors.cyan 0.6;
@@ -136,8 +136,8 @@ in {
 
   ui = {
     findHighlight = mixColors colors.base colors.highlight 0.4;
-    selection = mixColors colors.surface2 colors.purple 0.3;
-    cursor = colors.text;
+    selection = mixColors colors.surface2 colors.base00 0.6;
+    cursor = colors.magenta;
     activeBorder = colors.blue;
   };
 
