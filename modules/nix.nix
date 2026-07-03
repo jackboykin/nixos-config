8fca4303987c5e38c5652f328a847b26ef62ae15
Jack Boykin <jtboykin.jb@gmail.com>
2026-07-03 15:15:35 -0500

    nix: enable build cgroups and auto-allocated uids, move optimise to timer

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,32 +1,39 @@
 _: {
-  nix.channel.enable = false;
+  nix = {
+    channel.enable = false;
+    optimise.automatic = true;
 
-  nix.settings = {
-    experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
-    allowed-users = [
-      "root"
-      "@wheel"
-    ];
-    trusted-users = ["root"];
-    auto-optimise-store = true;
-    substituters = [
-      "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
-      "https://ryoppippi.cachix.org"
-    ];
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "ryoppippi.cachix.org-1:b2LbtWNvJeL/qb1B6TYOMK+apaCps4SCbzlPRfSQIms="
-    ];
-    builders-use-substitutes = true;
-    use-xdg-base-directories = true;
-    cores = 32;
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "cgroups"
+        "auto-allocate-uids"
+      ];
+      use-cgroups = true;
+      auto-allocate-uids = true;
+      allowed-users = [
+        "root"
+        "@wheel"
+      ];
+      flake-registry = "";
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://ryoppippi.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "ryoppippi.cachix.org-1:b2LbtWNvJeL/qb1B6TYOMK+apaCps4SCbzlPRfSQIms="
+      ];
+      use-xdg-base-directories = true;
+      cores = 32;
+    };
   };
 
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.permittedInsecurePackages = ["pnpm-10.29.2"];
+  nixpkgs.config = {
+    allowUnfree = true;
+    permittedInsecurePackages = ["pnpm-10.29.2"];
+  };
 }
