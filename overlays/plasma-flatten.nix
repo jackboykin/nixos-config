e7ac6719633aafc58049efbdb7defc1825c40331
Jack Boykin <jtboykin.jb@gmail.com>
2026-07-03 09:50:06 -0500

    plasma: hoist flatten forest into parallel derivation

--- overlays/plasma-flatten.nix
+++ overlays/plasma-flatten.nix
@@ -1,14 +1,55 @@
-final: prev: {
-  kdePackages = prev.kdePackages.overrideScope (kdeFinal: kdePrev: {
-    plasma-workspace = kdePrev.plasma-workspace.overrideAttrs (old: {
-      nativeBuildInputs = (old.nativeBuildInputs or []) ++ [final.lndir];
+final: prev: let
+  inherit (final) lib;
+
+  subdirs = {
+    NIXPKGS_QT6_QML_IMPORT_PATH = "lib/qt-6/qml";
+    QT_PLUGIN_PATH = "lib/qt-6/plugins";
+    XDG_CONFIG_DIRS = "etc/xdg";
+    XDG_DATA_DIRS = "share";
+  };
+
+  flattenQtPaths = pkg: let
+    deps =
+      lib.filter lib.isDerivation
+      (lib.closePropagation ((pkg.buildInputs or []) ++ (pkg.propagatedBuildInputs or [])));
+
+    manifest =
+      lib.concatLines (lib.unique (lib.flatten
+          (lib.mapAttrsToList (var: sub: map (d: "${var} ${d.bin or d.out or d}/${sub}") deps) subdirs)));
+
+    forest =
+      final.runCommand "${pkg.name}-flattened" {
+        nativeBuildInputs = [final.lndir final.shared-mime-info final.desktop-file-utils];
+        inherit manifest;
+        passAsFile = ["manifest"];
+      } ''
+        while read -r var dir; do
+          if [[ -d $dir ]]; then
+            mkdir -p "$out/$var"
+            lndir -silent "$dir" "$out/$var" || true
+          fi
+        done <"$manifestPath"
+        if [[ -d $out/XDG_DATA_DIRS/mime/packages ]]; then
+          find "$out/XDG_DATA_DIRS/mime" -mindepth 1 -maxdepth 1 ! -name packages -exec rm -r {} +
+          update-mime-database "$out/XDG_DATA_DIRS/mime"
+        fi
+        if [[ -d $out/XDG_DATA_DIRS/applications ]]; then
+          rm -f "$out/XDG_DATA_DIRS/applications/mimeinfo.cache"
+          update-desktop-database "$out/XDG_DATA_DIRS/applications"
+        fi
+        rm -f "$out"/XDG_DATA_DIRS/icons/*/icon-theme.cache
+      '';
+  in
+    pkg.overrideAttrs (old: {
+      flattenManifest = manifest;
+      passAsFile = (old.passAsFile or []) ++ ["flattenManifest"];
       preFixup =
         (old.preFixup or "")
         + ''
-          flatVars="XDG_DATA_DIRS XDG_CONFIG_DIRS QT_PLUGIN_PATH NIXPKGS_QT6_QML_IMPORT_PATH"
+          flatVars="${toString (lib.attrNames subdirs)}"
           keptArgs=()
           ownArgs=()
-          declare -A merged
+          sawData=
           i=0
           while ((i < ''${#qtWrapperArgs[@]})); do
             if [[ ''${qtWrapperArgs[i]} == --prefix && " $flatVars " == *" ''${qtWrapperArgs[i + 1]} "* ]]; then
@@ -16,8 +57,12 @@ final: prev: {
               dir=''${qtWrapperArgs[i + 3]}
               if [[ $dir == "$out"/* ]]; then
                 ownArgs+=(--prefix "$var" : "$dir")
+              elif grep -qxF "$var $dir" "$flattenManifestPath"; then
+                if [[ $var == XDG_DATA_DIRS ]]; then
+                  sawData=1
+                fi
               else
-                merged[$var]="$dir"$'\n'"''${merged[$var]-}"
+                keptArgs+=(--prefix "$var" : "$dir")
               fi
               ((i += 4))
             else
@@ -25,20 +70,21 @@ final: prev: {
               ((i += 1))
             fi
           done
+          [[ -n $sawData ]] || {
+            echo "flattenQtPaths: parsed no XDG_DATA_DIRS wrapper args; hook layout changed" >&2
+            exit 1
+          }
           qtWrapperArgs=("''${keptArgs[@]}")
           for var in $flatVars; do
-            [[ -n ''${merged[$var]-} ]] || continue
-            flat=$out/flattened/$var
-            mkdir -p "$flat"
-            while read -r dir; do
-              if [[ -d $dir ]]; then
-                lndir -silent "$dir" "$flat" || true
-              fi
-            done <<<"''${merged[$var]}"
-            qtWrapperArgs+=(--prefix "$var" : "$flat")
+            if [[ -d ${forest}/$var ]]; then
+              qtWrapperArgs+=(--prefix "$var" : "${forest}/$var")
+            fi
           done
           qtWrapperArgs+=("''${ownArgs[@]}")
         '';
     });
+in {
+  kdePackages = prev.kdePackages.overrideScope (kdeFinal: kdePrev: {
+    plasma-workspace = flattenQtPaths kdePrev.plasma-workspace;
   });
 }
