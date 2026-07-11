96d33378cb97ffb3a53c33fcc61e132876bfd115
Jack Boykin <jtboykin.jb@gmail.com>
2026-07-10 20:55:59 -0500

    nix: set max-jobs=4 and cores=8

--- modules/nix.nix
+++ modules/nix.nix
@@ -28,7 +28,8 @@ _: {
         "ryoppippi.cachix.org-1:b2LbtWNvJeL/qb1B6TYOMK+apaCps4SCbzlPRfSQIms="
       ];
       use-xdg-base-directories = true;
-      cores = 32;
+      max-jobs = 4;
+      cores = 8;
     };
   };
 
