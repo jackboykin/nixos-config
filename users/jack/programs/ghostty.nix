5ff74163ef02b3cf269ff895f0be8b04cc8edf4e
Jack Boykin <jtboykin.jb@gmail.com>
2026-07-29 13:50:10 -0500

    ghostty: add module with bellatrix theme and low-dpi hinting

---
+++ users/jack/programs/ghostty.nix
@@ -0,0 +1,28 @@
+{
+  lib,
+  theme,
+  ...
+}: let
+  inherit (theme) colors fonts ui;
+in {
+  programs.ghostty = {
+    enable = true;
+
+    settings = {
+      theme = "bellatrix";
+      font-family = fonts.mono.name;
+      font-size = fonts.size.normal;
+      freetype-load-flags = "no-light,no-autohint";
+      window-decoration = "none";
+      window-width = 143;
+      window-height = 41;
+    };
+
+    themes.bellatrix = {
+      palette = map (i: "${toString i}=${colors."color${toString i}"}") (lib.range 0 15);
+      inherit (colors) background foreground;
+      cursor-color = ui.cursor;
+      selection-background = ui.selection;
+    };
+  };
+}
