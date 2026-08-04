5d6606ce7bb9d5ed532f8e5243d937e0204854cf
Jack Boykin <jtboykin.jb@gmail.com>
2026-08-03 23:40:57 -0500

    nix: drop cachix substituters, cache.nixos.org default suffices

--- modules/nix.nix
+++ modules/nix.nix
@@ -24,16 +24,6 @@
         "@wheel"
       ];
       flake-registry = "";
-      substituters = [
-        "https://cache.nixos.org"
-        "https://nix-community.cachix.org"
-        "https://ryoppippi.cachix.org"
-      ];
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "ryoppippi.cachix.org-1:b2LbtWNvJeL/qb1B6TYOMK+apaCps4SCbzlPRfSQIms="
-      ];
       use-xdg-base-directories = true;
       max-jobs = 2;
       cores = 16;
