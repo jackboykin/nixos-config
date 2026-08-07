fc39322c91764137f9ddb49feac5f25b107d2332
Jack Boykin <jtboykin.jb@gmail.com>
2026-08-07 14:42:40 -0500

    overlays: compile plasma without qtweb

--- overlays/overlays.nix
+++ overlays/overlays.nix
@@ -4,6 +4,7 @@ inputs: [
   (import ./claude-code.nix inputs.claude-code)
   (import ./ffmpeg.nix inputs.ffmpeg-master)
   (import ./firefox-nightly.nix inputs)
+  (import ./qtweb.nix)
   (import ./plasma-flatten.nix)
   (import ./zig.nix inputs.zig-index)
 ]
